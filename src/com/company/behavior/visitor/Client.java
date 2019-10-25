35d1e827ef7665e6eacb4a6921333409b5053469
李俊锋 <673717615@qq.com>
2019-10-25 09:43:44 +0800

    行为型模式

---
+++ src/com/company/behavior/visitor/Client.java
@@ -0,0 +1,72 @@
+package com.company.behavior.visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+abstract class Element {
+    public abstract void accept(IVisitor visitor);
+    public abstract void doSomething();
+}
+
+interface IVisitor {
+    public void visit(ConcreteElement1 el1);
+    public void visit(ConcreteElement2 el2);
+}
+
+class ConcreteElement1 extends Element {
+    public void doSomething(){
+        System.out.println("这是元素1");
+    }
+
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class ConcreteElement2 extends Element {
+    public void doSomething(){
+        System.out.println("这是元素2");
+    }
+
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+}
+class Visitor implements IVisitor {
+
+    public void visit(ConcreteElement1 el1) {
+        el1.doSomething();
+    }
+
+    public void visit(ConcreteElement2 el2) {
+        el2.doSomething();
+    }
+}
+
+class ObjectStruture {
+    public static List<Element> getList(){
+        List<Element> list = new ArrayList<Element>();
+        Random ran = new Random();
+        for(int i=0; i<10; i++){
+            int a = ran.nextInt(100);
+            if(a>50){
+                list.add(new ConcreteElement1());
+            }else{
+                list.add(new ConcreteElement2());
+            }
+        }
+        return list;
+    }
+}
+
+public class Client {
+    public static void main(String[] args){
+        List<Element> list = ObjectStruture.getList();
+        for(Element e: list){
+            e.accept(new Visitor());
+        }
+    }
+}
+
+
