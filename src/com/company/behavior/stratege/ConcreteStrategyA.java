358bbe50fc995eb8601aacb928503074e9282acf
lijunfeng <673717615@qq.com>
2019-10-25 21:56:32 +0800

    策略模式

---
+++ src/com/company/behavior/stratege/ConcreteStrategyA.java
@@ -0,0 +1,25 @@
+package com.company.behavior.stratege;
+
+//具体策略类
+public class ConcreteStrategyA implements Strategy {
+    @Override
+    public void strategyInterface() {
+
+    }
+}
+
+class ConcreteStrategyB implements Strategy {
+
+    @Override
+    public void strategyInterface() {
+
+    }
+}
+
+class ConcreteStrategyC implements Strategy {
+
+    @Override
+    public void strategyInterface() {
+
+    }
+}
---
+++ src/com/company/behavior/stratege/Strategy.java
@@ -0,0 +1,9 @@
+package com.company.behavior.stratege;
+
+//抽象策略类
+public interface Strategy {
+    /**
+     * 策略方法
+     */
+    void strategyInterface();
+}
