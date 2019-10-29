e5fd0addc91d4dd7c0399e9da516bc4ed85e9064
lijunfeng <673717615@qq.com>
2019-10-29 23:01:32 +0800

    适配器模式

---
+++ src/com/company/structure/adapter/Adaptee.java
@@ -0,0 +1,8 @@
+package com.company.structure.adapter;
+
+/**
+ * 源角色
+ */
+public class Adaptee {
+    public void sampleOperation1(){}
+}
---
+++ src/com/company/structure/adapter/Target.java
@@ -0,0 +1,15 @@
+package com.company.structure.adapter;
+
+/**
+ * 目标角色的源代码
+ */
+public interface Target {
+    /**
+     * 这是源类Adaptee也有的方法
+     */
+    void sampleOperation1();
+    /**
+     * 这是源类Adapteee没有的方法
+     */
+    void sampleOperation2();
+}
